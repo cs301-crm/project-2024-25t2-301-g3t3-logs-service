9b353c893d630ee65c74d7223f31ac67278b7fd5
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-04-10 21:08:40 +0800

    feat: handle bad req params

---
+++ src/main/java/com/cs301/crm/exceptions/handlers/ApiExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.cs301.crm.exceptions.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.time.ZonedDateTime;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<ErrorResponse> handleException() {
+        return new ResponseEntity<>(
+                new ErrorResponse(false,"Something went wrong on our end. Try again.",
+                        HttpStatus.SERVICE_UNAVAILABLE,
+                        ZonedDateTime.now()
+                ), HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class})
+    public ResponseEntity<ErrorResponse> handleInvalidPageParameters() {
+        return new ResponseEntity<>(
+                new ErrorResponse(false,"Please enter only numbers for page and limit parameters.",
+                        HttpStatus.BAD_REQUEST,
+                        ZonedDateTime.now()
+                ), HttpStatus.BAD_REQUEST);
+    }
+
+}
