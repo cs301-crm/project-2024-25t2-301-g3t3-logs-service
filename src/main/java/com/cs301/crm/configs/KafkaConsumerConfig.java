f8b1452ae21c2662de9f73e9493b29148017dfbc
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-04-11 15:04:29 +0800

    feat: add configs for prod

--- src/main/java/com/cs301/crm/configs/KafkaConsumerConfig.java
+++ src/main/java/com/cs301/crm/configs/KafkaConsumerConfig.java
@@ -1,6 +1,7 @@
 package com.cs301.crm.configs;
 
-import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializer;
+import com.amazonaws.services.schemaregistry.deserializers.GlueSchemaRegistryKafkaDeserializer;
+import com.amazonaws.services.schemaregistry.utils.AWSSchemaRegistryConstants;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,8 +11,8 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-
-import com.cs301.shared.protobuf.Log;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.glue.model.DataFormat;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,24 +26,27 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.consumer.properties.schema.registry.url}")
     private String schemaRegistryUrl;
 
+    @Value("{spring.kafka.consumer.schema.log}")
+    private String protobufLogSchema;
+
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "log-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaProtobufDeserializer.class.getName());
-        props.put("schema.registry.url", schemaRegistryUrl);
-        props.put("specific.protobuf.value.type", Log.class.getName());
+//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaProtobufDeserializer.class.getName());
+//        props.put("schema.registry.url", schemaRegistryUrl);
+//        props.put("specific.protobuf.value.type", Log.class.getName());
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
 
         // prod
-//                props.put(ProducerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GlueSchemaRegistryKafkaSerializer.class.getName());
-//        props.put(AWSSchemaRegistryConstants.AWS_REGION, Region.AP_SOUTHEAST_1);
-//        props.put(AWSSchemaRegistryConstants.DATA_FORMAT, DataFormat.PROTOBUF.name());
-//        props.put(AWSSchemaRegistryConstants.REGISTRY_NAME, schemaRegistry);
-//        props.put(AWSSchemaRegistryConstants.SCHEMA_NAME, protobufLogSchema);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GlueSchemaRegistryKafkaDeserializer.class.getName());
+        props.put(AWSSchemaRegistryConstants.AWS_REGION, Region.AP_SOUTHEAST_1);
+        props.put(AWSSchemaRegistryConstants.DATA_FORMAT, DataFormat.PROTOBUF.name());
+        props.put(AWSSchemaRegistryConstants.REGISTRY_NAME, schemaRegistryUrl);
+        props.put(AWSSchemaRegistryConstants.SCHEMA_NAME, protobufLogSchema);
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
